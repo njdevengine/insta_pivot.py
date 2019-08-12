9b0167d6d674341400babb2cb063536e1054769a
njdevengine <newjerseydevelopment@gmail.com>
2019-08-12 10:09:15 -0400

    Update insta_pivot.py

--- insta_pivot.py
+++ insta_pivot.py
@@ -2,7 +2,7 @@
 
 from os import listdir
 from os.path import isfile, join
-mypath = r'C:\#hashtag\\'
+mypath = r'mypath\#hashtag\\'
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 jsons = []
